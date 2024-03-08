2c3b3589a82a265df326f2dde64363105a2f4041
Yelhie <maelle.ew.m@gmail.com>
2024-03-08 15:28:34 +0100

    Ajout de l'affichage des données quand l'user est connecté
    - Modification des la pages userDétails et du composant Account afin d'afficher les données de l'utilisateur lors de sa connection.
    - Ajout d'un fichier json pour les données

--- frontend/src/pages/User/UserDetailsPage.jsx
+++ frontend/src/pages/User/UserDetailsPage.jsx
@@ -1,13 +1,34 @@
 import Account from '../../components/Account/Account';
+import Data from "../../data/Data.json";
+
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom';
 
 function UserDetails() {
-    return (
-        <div className="header">
-            <h1>Welcome back<br />Tony Jarvis!</h1>
-            <button className="edit-button">Edit Name</button>
-            <Account />
-        </div>
+    const selectUser = (state) => state.user.user.body
+    const user = useSelector(selectUser)
+    const navigate = useNavigate();
 
+    if (!user) {
+        return navigate('/');
+    }
+
+    return (
+        <main className="main bg-dark main-user">
+            <div className="header">
+                <h1>
+                    Welcome back
+                    <br />
+                    {user.firstName} {user.lastName}
+                </h1>
+                <button className="edit-button">Edit Name</button>
+                <h2 className="sr-only">Accounts</h2>
+            </div>
+            <h2 className="sr-only">Accounts</h2>
+            {Data.map((account, index) => (
+                <Account key={`account-${index}`} title={account.title} amount={account.amount} description={account.description} />
+            ))}
+        </main>
     );
 }
 
